fafc3eead109f16067aa721a2690da6180e06ab5
Pawel Wasil <wasil.paul@gmail.com>
2018-07-10 11:55:46 +0200

    Cheerio and node-fetch lib added

--- js-scraper.js
+++ js-scraper.js
@@ -1,6 +1,18 @@
 const fetch = require('node-fetch')
 
 const url = `http://kodeturbo.com/index.php`
-fetch(`${url}?do=turbo&oem=767378-0013`)
-    .then(resp => resp.text())
-    .then(console.log)
+
+const searchTurbin = `?do=turbo&oem=` // turbin search url+search+idNumber
+
+const allMarks = []  //?marka=Chevrolet&do=cars
+
+
+function searchMarks(mark) {
+    const searchingMark = `?marka=`
+    const car = `&do=cars`
+    return fetch(`${url}${searchingMark}${mark}${car}`)
+        .then(resp => resp.text())
+}
+
+
+searchMarks(`Alfa-Romeo`).then(console.log)
