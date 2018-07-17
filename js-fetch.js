4b1e9906fb82942871bbe3a63b541aa7c85280ec
Pawel Wasil <wasil.paul@gmail.com>
2018-07-17 15:00:22 +0200

    All marks name and each parameter fetched

--- js-fetch.js
+++ js-fetch.js
@@ -2,23 +2,46 @@ const fetch = require('node-fetch')
 const cheerio = require('cheerio')
 
 const url = [`http://kodeturbo.com/index.php`, `?marka=`, `?do=cars2&marka=`, `&do=cars`, `&model=`, `?do=turbo&oem=`]
+const allBrands = []
+let allMarks = []
 
-async function getMA() {
-
-    const allMarks = await fetch(`${url[0]}`)
+async function getBrands() {
+    const brands = await fetch(`${url[0]}`)
         .then(resp => resp.text())
-
-    const $ = cheerio.load(allMarks)
-    
-    $(`option`).each(function (i, mark) {
-        const $mark = $(mark)
-        const bk = []
-        bk.push($mark.text())
-        console.log(bk)
-        return bk
+    const $ = cheerio.load(brands)
+    $(`option`).each(function (i, brand) {
+        const $brand = $(brand)
+        allBrands.push($brand.text())
+        return allBrands
     })
-    
+    console.log(allBrands)
 }
 
 
-getMA()
+function getModel() {
+    const inAllMarks = []
+    allBrands.forEach(async function (brand) {
+
+        const mark = await fetch(`${url[0]}${url[1]}${brand}${url[3]}`).then(resp => resp.text())
+        const $ = cheerio.load(mark)
+        $(`tr`).each(function (i, mark) {
+            if (i != 0) {
+                const $mark = $(mark)
+                inAllMarks.push($mark.text().split('\n'))
+            }
+            return inAllMarks
+        })
+        allMarks = inAllMarks.map(x =>
+            x.map(x =>
+                x.trim()).filter(x => x != ''))
+        console.log(allMarks)
+
+    })
+}
+
+async function getALL() {
+    await getBrands()
+    await getModel()
+}
+
+getALL()
