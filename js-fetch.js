ee8a7220d2364939f88bd6030621e1566926186e
Pawel Wasil <wasil.paul@gmail.com>
2018-07-18 22:02:28 +0200

    All turbo_OEM data saved

--- js-fetch.js
+++ js-fetch.js
@@ -17,7 +17,7 @@ async function getBrands() {
 
         return allBrands
     })
-    fs.writeFile(`./brands.json`, JSON.stringify(allBrands, null, 2))
+    fs.writeFile(`./data/Brands.json`, JSON.stringify(allBrands, null, 2))
     console.log(allBrands)
 }
 
@@ -39,36 +39,17 @@ function getModels() {
                     x.trim()).filter(x => x != ''))
             .map((x, i) => { return { nr: i, marka: x[0], model: x[1].replace(/ /gi, '%20') } })
         console.log(allModels)
-        fs.writeFile(`./models.json`, JSON.stringify(allModels, null, 2))
+        fs.writeFile(`./data/Models.json`, JSON.stringify(allModels, null, 2))
         return allModels
 
     })
 }
 //http://kodeturbo.com/index.php?do=cars2&marka=Alfa-Romeo&model=145%201.9%20JTD
 
-function getModelsParameters() {
-    inAllParameters = []
-    allModels.forEach(async function (model) {
-        const parameter = await fetch(`${url[0]}${url[2]}${model.marka}${url[4]}${model.model}`).then(resp => resp.text())
-        console.log(parameter)
-        const $ = cheerio.load(parameter)
-        $(`tr`).each(function (i, parameter) {
-            const $parameter = $(parameter)
-            if (i != 0) {
-                inAllParameters.push($parameter.text())
-            }
-            console.log(inAllParameters)
-            fs.writeFile(`./Parameters.json`, JSON.stringify(inAllParameters, null, 2))
-            return inAllParameters
-        })
-
-    })
-}
-
 async function getALL() {
     await getBrands()
     await getModels()
-    // await getModelsParameters()
+
 
 }
 
--- open-file.js
+++ open-file.js
@@ -4,19 +4,20 @@ const cheerio = require('cheerio')
 
 const url = [`http://kodeturbo.com/index.php`, `?marka=`, `?do=cars2&marka=`, `&do=cars`, `&model=`, `?do=turbo&oem=`]
 
-const data = fs.readFileSync(`./Models.json`)
+const data = fs.readFileSync(`./data/Models.json`)
 
 const jsonData = JSON.parse(data)
 
 const show = (arr) => arr.map(arr => console.log(arr))
 
+let modelsParameters = []
 //show(jsonData)
 
 function getModelsParameters(data) {
     inAllParameters = []
     if (data && data.length) {
-        data/*.filter(x => x.marka == 'DDC')*/.forEach(async function (marka) {
-            const parameter = await fetch(`${url[0]}${url[2]}${marka.marka}${url[4]}${marka.model}`).then(resp => resp.text()).catch(error => console.log(error))
+        data/*.filter(x => x.marka == `Alfa-Romeo`||'Audi'||`Alpina`)*/.forEach(async function (marka) {
+            const parameter = await fetch(`${url[0]}${url[2]}${marka.marka}${url[4]}${marka.model}`).then(resp => resp.text()).catch(error => console.log(`ERROR: `, error))
             const $ = cheerio.load(parameter)
 
             $(`tr`).each(function (i, parameter) {
@@ -27,17 +28,23 @@ function getModelsParameters(data) {
                 console.log(inAllParameters)
 
             })
-            fs.writeFile(`./Parameters.json`, JSON.stringify(inAllParameters, null, 2))
-            return inAllParameters
+            modelsParameters = inAllParameters.map(x => {
+
+                return {
+                    mark: x[1].trim(),
+                    model: x[2].trim(),
+                    power: x[3].trim(),
+                    capacity: x[4].trim(),
+                    no: x[5].trim(),
+                    date: x[6].trim(),
+                    turbo_OEM: x.slice(7, x.length).map(x => x.trim()).filter(x => x != '')
+                }
+            })
+            fs.writeFile(`./data/Parameters.json`, JSON.stringify(modelsParameters, null, 2))
+            return modelsParameters
         })
     }
 }
 
-//getModelsParameters(jsonData)
-const params = fs.readFileSync(`./Parameters.json`)
+getModelsParameters(jsonData)
 
-const jsonParams = JSON.parse(params)
-
-const showLen = jsonParams.length
-
-console.log(showLen)
