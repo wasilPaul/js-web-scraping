6b351bb258c5d327118d1c44de0495341794009b
Pawel Wasil <wasil.paul@gmail.com>
2018-07-19 12:54:40 +0200

    Scraper finish

---
+++ js_scraper_dataConvert.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const data = fs.readFileSync(`./data/Parameters.json`)
+
+const jsonData = JSON.parse(data)
+
+const turboNumbers = []
+
+const getAllTurboNumber = allParameters =>
+  allParameters
+    .reduce((allNr, modelNr) =>
+      allNr.concat(...modelNr.turbo_OEM), [])
+    .filter((nr, idx, allArray) => allArray.indexOf(nr) == idx)
+    .sort()
+    .map(x => x.replace(/ /gi, `%`))
+    .map(auto => turboNumbers.push(auto));
+
+getAllTurboNumber(jsonData)
+
+console.log(turboNumbers)
+
+fs.writeFile(`./data/TurboNrOEM.json`, JSON.stringify(turboNumbers, null, 2))
+
+
---
+++ js_scraper_getModelsParameters.js
@@ -0,0 +1,50 @@
+let fs = require(`fs`)
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+
+const url = [`http://kodeturbo.com/index.php`, `?marka=`, `?do=cars2&marka=`, `&do=cars`, `&model=`, `?do=turbo&oem=`]
+
+const data = fs.readFileSync(`./data/Models.json`)
+
+const jsonData = JSON.parse(data)
+
+let modelsParameters = []
+
+function getModelsParameters(data) {
+  let inAllParameters = []
+  if (data && data.length) {
+    data.forEach(async function (marka) {
+      try {
+        const parameter = await fetch(`${url[0]}${url[2]}${marka.marka}${url[4]}${marka.model}`).then(resp => resp.text())
+        const $ = cheerio.load(parameter)
+
+        $(`tr`).each(function (i, parameter) {
+          const $parameter = $(parameter)
+          if (i != 0) {
+            inAllParameters.push($parameter.text().split('\n'))
+          }
+          console.log(inAllParameters)
+        })
+
+        modelsParameters = inAllParameters.map(x => {
+          return {
+            mark: x[1].trim(),
+            model: x[2].trim(),
+            power: x[3].trim(),
+            capacity: x[4].trim(),
+            no: x[5].trim(),
+            date: x[6].trim(),
+            turbo_OEM: x.slice(7, x.length).map(x => x.trim()).filter(x => x != '')
+          }
+        })
+        //fs.writeFile(`./data/Parameters.json`, JSON.stringify(modelsParameters, null, 2))
+        return modelsParameters
+      } catch (error) {
+        console.log(`message Error: ${error}`);
+      }
+    })
+  }
+}
+
+getModelsParameters(jsonData)
+
