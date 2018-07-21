68bcdcb5c450a02803c23a89568be305b476dd90
Pawel Wasil <wasil.paul@gmail.com>
2018-07-21 22:34:49 +0200

    Simple working scraper with request and cheerio lib - test ok

--- _scraper_node.js
+++ _scraper_node.js
@@ -1,5 +1,21 @@
 const cheerio = require(`cheerio`)
-const express = require(`expresss`)
-const fs = require(`fs`)
-const path = require(`path`)
 const request = require(`request`)
+
+const url = `http://kodeturbo.com/index.php`
+
+request(url, function (error, resp, body) {
+    if (error) {
+        return console.error('upload failed:', error);
+    }
+
+    const allBrands = []
+    const $ = cheerio.load(body)
+    let brand = $(`option`).each(function (i, brand) {
+        const $brand = $(brand)
+        allBrands.push($brand.text())
+        return allBrands
+    })
+    console.log(allBrands)
+    console.log('statusCode:', resp && resp.statusCode)
+})
+
